177f49b85133b63f0e24445c4c4a778f90461b4c
zwe.htatnaing@reditechasia.com <56188320+zwemanutechs@users.noreply.github.com>
2019-12-05 15:34:31 +0800

    Updates

---
+++ precache-manifest.13a047c980afa7b2085ca3a212789be9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8578a1618adb778fe7feb3c55545ac4f",
+    "url": "//zwehtatnaing-reditechasia.github.io/TestCameraApp_1/index.html"
+  },
+  {
+    "revision": "c854d6423719850574d1",
+    "url": "//zwehtatnaing-reditechasia.github.io/TestCameraApp_1/static/css/2.f0405d8a.chunk.css"
+  },
+  {
+    "revision": "4e5c460ad8c13ec3f28c",
+    "url": "//zwehtatnaing-reditechasia.github.io/TestCameraApp_1/static/css/main.8652d4e9.chunk.css"
+  },
+  {
+    "revision": "c854d6423719850574d1",
+    "url": "//zwehtatnaing-reditechasia.github.io/TestCameraApp_1/static/js/2.f2b1b3f0.chunk.js"
+  },
+  {
+    "revision": "e2fc3609d47d90b8cc004bfe15ddf15e",
+    "url": "//zwehtatnaing-reditechasia.github.io/TestCameraApp_1/static/js/2.f2b1b3f0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4e5c460ad8c13ec3f28c",
+    "url": "//zwehtatnaing-reditechasia.github.io/TestCameraApp_1/static/js/main.c82423a5.chunk.js"
+  },
+  {
+    "revision": "71a80bb700eddd7bfe5f",
+    "url": "//zwehtatnaing-reditechasia.github.io/TestCameraApp_1/static/js/runtime-main.0a5760f0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "//zwehtatnaing-reditechasia.github.io/TestCameraApp_1/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4b1287b9af362426b38e37aee4a16015",
+    "url": "//zwehtatnaing-reditechasia.github.io/TestCameraApp_1/static/media/openCamera_3.4b1287b9.png"
+  }
+]);
