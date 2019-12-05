809b3a312ddc04d94d7089bbb931d335f4185ff3
zwehtatnaing-reditechasia <56244810+zwehtatnaing-reditechasia@users.noreply.github.com>
2019-12-05 15:49:17 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter, Link, Route, Switch } from 'react-router-dom'
+import logo from './logo.svg'
+import './App.css'
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  Hidden,
+  Drawer,
+  Divider,
+  List,
+  ListItem,
+  ListItemText,
+  Collapse
+} from '@material-ui/core'
 import StuffingContainer from './stuffingContainer/stuffingContainer'
 import StuffingContainerDynamic from './stuffingContainerDynamic/stuffingContainerDynamic'
 
-function App() {
+function App () {
   return (
-    <div className="App" style={{padding: 15}}>
+    <div className='App' style={{ padding: 15 }}>
+      <List component='div'>
+        <ListItem
+          button
+          component={Link}
+          to='/StuffingContainer'
+        >
+          <ListItemText primary='StuffingContainer' />
+        </ListItem>
+        <Divider />
+        <ListItem
+          button
+          component={Link}
+          to='/StuffingContainerDynamic'
+        >
+          <ListItemText primary='StuffingContainerDynamic' />
+        </ListItem>
+      </List>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/StuffingContainer' render={() => <StuffingContainer />} />
+          <Route exact path='/StuffingContainerDynamic' render={() => <StuffingContainerDynamic />} />
+        </Switch>
+      </BrowserRouter>
       <StuffingContainerDynamic />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
